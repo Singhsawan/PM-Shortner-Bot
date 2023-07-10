c52e9635f20695b0f0c8c43cd96ea2d52b57bcc8
Sneha Sharma <119250143+Sawansingh24@users.noreply.github.com>
2023-07-10 11:08:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/TeamHMT/ben-url-filter-bot.git /ben-url-filter-bot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /ben-url-filter-bot
-fi
-cd /ben-url-filter-bot
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+gunicorn app:app & python3 bot.py
