f84b554b32b7d40a45967249d26c26d80400c75e
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-07-14 12:12:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '29362464'))
-API_HASH = environ.get('API_HASH', '31973315b0872a0478886de31a1e4848')
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
