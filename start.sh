04e48e6c8bea212c45710a7383470c3f9df98eb5
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-09-02 10:46:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/iwhiwh/PM-Shortner-Bot /PM-Shortner-Bot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PM-Shortner-Bot
-fi
-cd /Elsa
-pip3 install -U -r requirements.txt
-echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
-python3 bot.py
+gunicorn app:app & python3 bot.py
