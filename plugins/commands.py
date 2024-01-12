bac4054c0f35a1f895fbf057f4a9c745e1b136c8
Jack <125857863+Singhsawan@users.noreply.github.com>
2024-01-12 10:27:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -165,15 +165,14 @@ async def start(client, message):
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
-                    InlineKeyboardMarkup(
+                    reply_markup=InlineKeyboardMarkup(
                         [
-                            [
-                                InlineKeyboardButton('🌈 Wᴀᴛᴄʜ Oɴʟɪɴᴇ / Fᴀꜱᴛ Dᴏᴡɴʟᴏᴀᴅ 🌈', callback_data=f'gen_stream_link:{file_id}')
-                            ]
+                         [
+                          InlineKeyboardButton("✨ᴍᴏᴠɪᴇꜱ ɢʀᴏᴜᴘ✨", url="https://t.me/+4nzja42ELQwzOWVl")
+                         ]
                         ]
                     )
-                    )
-               )
+                )
             except Exception as e:
                 logger.warning(e, exc_info=True)
                 continue
