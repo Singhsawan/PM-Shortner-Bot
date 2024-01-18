34916015531b7430a2dfea516c0590ab7b79a6d3
doxadeve <156528676+doxadeve@users.noreply.github.com>
2024-01-18 11:18:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1040,3 +1040,13 @@ def get_name(media_msg: Message) -> str:
 def get_hash(media_msg: Message) -> str:
     media = get_media_from_message(media_msg)
     return getattr(media, "file_unique_id", "")[:6]
+
+
+async def get_cap(settings, remaining_seconds, files, query, total_results, search):
+    
+        cap = f"<b>Hᴇʏ {query.from_user.mention}, Fᴏᴜɴᴅ {total_results} Rᴇsᴜʟᴛs ғᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {search}\n\n</b>"
+        for file in files:
+            cap += f"<b>♻️ <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
+        return cap
+
+
